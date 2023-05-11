96b992f13e92fb36129c1affccf803d4208c1d32
hayaman3 <kevin.haya@yahoo.com>
2023-05-11 17:40:23 +0800

    Created Form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
 
 export default function Home() {
   return  (
     <Container maxW="450px" mt={12}>
       <Heading>Contact</Heading>
+      <FormControl isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input type="text" name="name"/>
+      </FormControl>
     </Container>
     );
 }
